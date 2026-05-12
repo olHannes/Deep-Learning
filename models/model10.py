aaadb75ddb9a9b3170d40b50ec0a7ffd4a261901
crossi262 <mariusroesner15@gmail.com>
2026-05-12 11:18:12 +0200

    quick fixes

--- models/model10.py
+++ models/model10.py
@@ -4,15 +4,12 @@ from tensorflow.keras import layers, Sequential
 def build_model(input_shape=(256, 256, 3), num_classes=10):
     model = Sequential()
     
-    # Erste Schicht: weniger Filter
     model.add(layers.Conv2D(32, (3,3), activation="relu", input_shape=input_shape))
     model.add(layers.MaxPooling2D((2,2)))
     
-    # Zweite Schicht: reduziert
     model.add(layers.Conv2D(64, (3,3), activation="relu"))
     model.add(layers.MaxPooling2D((2,2)))
     
-    # Flatten und Dense Layer
     model.add(layers.Flatten())
     model.add(layers.Dense(128, activation="relu"))
     model.add(layers.Dropout(0.5))
--- models/model3.py
+++ models/model3.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers, Sequential
 
-def build_model(input_shape=(256,256,3), num_clases=10):
+def build_model(input_shape=(256, 256, 3), num_classes=10):
     model = Sequential()
 
     model.add(layers.Conv2D(64, (3,3), activation="relu", input_shape=input_shape))
