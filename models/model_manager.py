b4fc65650e71a15a83b332d37b01f9a3e2bdbea4
crossi262 <mariusroesner15@gmail.com>
2026-05-12 11:13:01 +0200

    Added the other model files

--- models/model_manager.py
+++ models/model_manager.py
@@ -4,15 +4,27 @@ from models import model2
 from models import model3
 from models import model10
 
-def get_model(model_choice=0, input_shape=(256,256,3), num_clases=10):
+def get_model(model_choice=0, input_shape=(256,256,3), num_classes=10):
     
     if model_choice == 1:
-        return model1.build_model(input_shape, num_clases)
+        return model1.build_model(input_shape, num_classes)
     elif model_choice == 2:
-        return model2.build_model(input_shape, num_clases)
+        return model2.build_model(input_shape, num_classes)
     elif model_choice == 3:
-        return model3.build_model(input_shape, num_clases)
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 4:
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 5:
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 6:
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 7:
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 8:
+        return model3.build_model(input_shape, num_classes)
+    elif model_choice == 9:
+        return model3.build_model(input_shape, num_classes)
     elif model_choice == 10:
-        return model10.build_model(input_shape, num_clases)
+        return model10.build_model(input_shape, num_classes)
     else:
         raise ValueError("Invalid model-choice: " , model_choice)
